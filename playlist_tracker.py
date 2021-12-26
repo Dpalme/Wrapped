df7365b12909db5f8b53aecde998377fbcd1716a
Dpalme <diegoPalmerin24@gmail.com>
2021-12-26 05:05:03 -0600

    examples

--- playlist_tracker.py
+++ playlist_tracker.py
@@ -1,5 +1,7 @@
 import wrapped
 import datetime
+import requests
+import base64
 from watching_playlists import playlists
 
 try:
@@ -13,10 +15,13 @@ except FileNotFoundError:
 with open(f'playlist_watch.txt', 'w', encoding='UTF-8') as archivo:
     for playlist in playlists:
         curr = wrapped.sp.playlist(playlist)
-        if curr['snapshot_id'] != previous[playlist]:
+        if playlist not in previous or curr['snapshot_id'] != previous[playlist]:
             wrapped.create_playlist(f'{curr["name"]} ({datetime.date.today()})')
             new_p = wrapped.sp.current_user_playlists(limit=1)['items'][0]['uri']
             wrapped.replace_tracks(wrapped.get_tracks_uri(playlist), new_p)
             if curr['description'] != '':
-                wrapped.sp.playlist_change_details(playlist, description=curr['description'])
+                wrapped.sp.playlist_change_details(new_p, description=curr['description'])
+            cover = wrapped.sp.playlist_cover_image(playlist)[0]['url']
+            img = base64.b64encode(requests.get(cover).content)
+            wrapped.sp.playlist_upload_cover_image(new_p, img)
         archivo.write(f'{playlist} {curr["snapshot_id"]}\n')
