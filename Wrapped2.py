1f626fe0cbc92dd7c6c5ea7f27adc67424047fe4
Dpalme <diegoPalmerin24@gmail.com>
2021-12-29 21:08:52 -0600

    genres for all tracks

--- Wrapped2.py
+++ Wrapped2.py
@@ -1,16 +1,13 @@
 import itertools
-
-from six import print_
 import spotipy_auth
 import json
 import concurrent.futures
 import collections
 DATA_LIMIT = 50
 
-
-class Wrapped(object):
+class Wrapped():
     def __init__(self, sp=spotipy_auth.sp):
-        # Makes a call to the Spotify API using Spotipy to get the access token
+        self.USER = spotipy_auth.USERNAME
         self.sp = sp
         self.playlists = {}
         self.albums = {}
@@ -23,9 +20,22 @@ class Wrapped(object):
             self.albums = database['albums']
             self.playlists = database['playlists']
             self.artists = database['artists']
+            self.genres = database['genres']
         except FileNotFoundError:
             self.update()
-        
+
+    def get_grouped_by_id(self, fn, ids, property_name=None):
+        ids = tuple(set(ids))
+        groupings = tuple(ids[(i*50):(i*50)+50]
+                          for i in range((len(ids)//50) + 1))
+        n = len(groupings) if len(groupings) < 50 else 50
+        with concurrent.futures.ThreadPoolExecutor(n) as ex:
+            return itertools.chain.from_iterable(
+                req.result()[property_name] if property_name else req.result()
+                for req in concurrent.futures.as_completed(
+                    {ex.submit(fn, group) for group in groupings})
+            )
+
     def get_data_by_id(self, fn, ids):
         ids = set(ids)
         n = len(ids) if len(ids) < 20 else 20
@@ -45,7 +55,7 @@ class Wrapped(object):
                 for req in concurrent.futures.as_completed(
                     {ex.submit(fn, DATA_LIMIT, n): n for n in reqs}))
 
-    def get_saved_albums(self):
+    def get_albums(self):
         self.albums = {a['album']['id']: {
             "name": a['album']['name'],
             "id": a['album']['id'],
@@ -61,12 +71,11 @@ class Wrapped(object):
                 for ar in a['album']['artists']},
             "total_tracks": a['album']['total_tracks'],
             "tracks": {
-                    t['id']: {
+                t['id']: {
                     'name': t['name'],
                     'id': t['id']}
-                 for t in a['album']['tracks']['items']}}
+                for t in a['album']['tracks']['items']}}
             for a in self.get_chained_data(self.sp.current_user_saved_albums)}
-        print(len(self.albums), 'albums')
         return self.albums
 
     def get_playlists(self):
@@ -75,12 +84,12 @@ class Wrapped(object):
                 'name': p['name'],
                 'id': p['id'],
                 'description': p['description'],
+                'snapshot_id': p['snapshot_id'],
                 'owner': {
                     'display_name': p['owner']['display_name'],
                     'uri': p['owner']['uri']
                 }}
             for p in self.get_chained_data(self.sp.current_user_playlists)}
-        print(len(self.playlists), 'playlists')
         return self.playlists
 
     def get_tracks(self):
@@ -91,7 +100,7 @@ class Wrapped(object):
             "release_date": t['track']['album']['release_date'],
             'added_date': t['added_at'],
             "isrc": t['track']['external_ids']['isrc'],
-            "artists": 
+            "artists":
                 {ar['id']: {
                     'name': ar['name'],
                     'id': ar['id'],
@@ -102,7 +111,6 @@ class Wrapped(object):
                 'id': t['track']['album']['id'],
                 'name': t['track']['album']['name']}
         } for t in self.get_chained_data(self.sp.current_user_saved_tracks)}
-        print(len(self.tracks), 'tracks')
         return self.tracks
 
     def get_artists(self):
@@ -113,17 +121,49 @@ class Wrapped(object):
             "uri": a['uri'],
             'genres': a['genres'],
             "followers": a['followers']['total']
-        } for a in self.get_data_by_id(
-            self.sp.artist,
-            {artist for album in self.albums.values() for artist in album['artists']})}
-        print(len(self.artists), 'artists')
+        } for a in self.get_grouped_by_id(
+            self.sp.artists,
+            {artist
+             for track in self.tracks.values()
+             for artist in track['artists']}, 'artists')}
         return self.artists
+
+    def populate_genres(self):
+        self.genres = collections.defaultdict(list)
+        for artist in self.artists.values():
+            tracks = tuple(
+                filter(lambda a: artist['id'] in a['artists'], self.tracks.values()))
+            for genre in artist['genres']:
+                for track in tracks:
+                    self.genres[genre].append(track['id'])
+        return self.genres
+
+    def _playlist_util(self, fn, tracks, playlist):
+        if len(tracks) > 100:
+            fn(self.USER, playlist, [])
+            for n in range(0, len(tracks), 100):
+                self.sp.user_playlist_add_tracks(
+                    self.USER, playlist, tracks[n:n+100])
+        else:
+            fn(self.USER, playlist, tracks)
+    
+    def add_tracks(self, tracks, playlist):
+        self._playlist_util(self.sp.user_playlist_add_tracks, tracks, playlist)
+
+    def replace_tracks(self, tracks, playlist):
+        self._playlist_util(self.sp.user_playlist_replace_tracks, tracks, playlist)
 
     def update(self):
         self.get_playlists()
-        self.get_saved_albums()
+        print(len(self.playlists), 'playlists')
+        self.get_albums()
+        print(len(self.albums), 'albums')
         self.get_tracks()
+        print(len(self.tracks), 'tracks')
         self.get_artists()
+        print(len(self.artists), 'artists')
+        self.populate_genres()
+        print(len(self.genres), 'genres')
         self.save()
 
     def save(self, filename='database'):
@@ -132,7 +172,8 @@ class Wrapped(object):
                 'playlists': self.playlists,
                 'albums': self.albums,
                 'tracks': self.tracks,
-                'artists': self.artists
+                'artists': self.artists,
+                'genres': self.genres
             },
                 indent=2,
                 sort_keys=False,
